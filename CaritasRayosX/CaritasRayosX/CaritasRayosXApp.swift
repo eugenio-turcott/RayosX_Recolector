810a8f67b116eb1264b787947ae41d1d14935f20
Eugenio Turcott <A01198189@tec.mx>
2023-09-06 11:29:38 -0600

    Creado Repositorio
    Se creo

---
+++ CaritasRayosX/CaritasRayosX/CaritasRayosXApp.swift
@@ -0,0 +1,17 @@
+//
+//  CaritasRayosXApp.swift
+//  CaritasRayosX
+//
+//  Created by Alumno on 06/09/23.
+//
+
+import SwiftUI
+
+@main
+struct CaritasRayosXApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

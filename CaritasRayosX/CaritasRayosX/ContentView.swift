6232947d1354a8e28f77bb15ad655c77d7116f04
GReyes4 <A00833852@tec.mx>
2023-09-06 11:59:36 -0600

    Add ListaDeRecibosView

--- CaritasRayosX/CaritasRayosX/ContentView.swift
+++ CaritasRayosX/CaritasRayosX/ContentView.swift
@@ -9,29 +9,8 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        NavigationStack{
-            VStack {
-                HStack {
-                    Spacer()
-                }
-                VStack {
-                    List(listaDonantes){
-                        donanteItem in
-                        NavigationLink{
-                            DetallesView(donante: donanteItem)
-                        }
-                    label:{
-                        donanteRow(donante: donanteItem)
-                    }
-                    }
-                    .listStyle(.inset)
-                }
-                .cornerRadius(10)
-                .padding()
-                Spacer()
-            }
-            .background(Color(red: 0/255, green: 156/255, blue: 166/255))
-        }
+        Text("Hola")
+        ListaDeRecibos()
     }
 }
 
---
+++ CaritasRayosX/CaritasRayosX/ListaDeRecibos.swift
@@ -0,0 +1,42 @@
+//
+//  ListaDeRecibos.swift
+//  CaritasRayosX
+//
+//  Created by Alumno on 06/09/23.
+//
+
+import SwiftUI
+
+struct ListaDeRecibos: View {
+    var body: some View {
+        NavigationStack{
+            VStack {
+                HStack {
+                    Spacer()
+                }
+                VStack {
+                    List(listaDonantes){
+                        donanteItem in
+                        NavigationLink{
+                            DetallesView(donante: donanteItem)
+                        }
+                    label:{
+                        donanteRow(donante: donanteItem)
+                    }
+                    }
+                    .listStyle(.inset)
+                }
+                .cornerRadius(10)
+                .padding()
+                Spacer()
+            }
+            .background(Color(red: 0/255, green: 156/255, blue: 166/255))
+        }
+    }
+}
+
+struct ListaDeRecibos_Previews: PreviewProvider {
+    static var previews: some View {
+        ListaDeRecibos()
+    }
+}

2435ddcac8ea67954503de01bcac8717af3c36e4
PatriciaDeLeon <108317756+PatriciaDeLeon@users.noreply.github.com>
2023-09-26 13:35:17 -0600

    Add files via upload

---
+++ CaritasRayosX/RayosXCordinador/RayosXCordinador/RayosXCordinadorApp.swift
@@ -0,0 +1,17 @@
+//
+//  RayosXCordinadorApp.swift
+//  RayosXCordinador
+//
+//  Created by Alumno on 26/09/23.
+//
+
+import SwiftUI
+
+@main
+struct RayosXCordinadorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

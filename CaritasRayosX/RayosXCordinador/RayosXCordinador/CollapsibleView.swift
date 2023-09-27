89f3f73ed5d1e398446ea914bd1d9217f06f8fd1
Ainhize Legarreta <ainhizelegarreta@gmail.com>
2023-09-26 18:27:07 -0600

    Collapsible View

--- CaritasRayosX/RayosXCordinador/RayosXCordinador/CollapsibleView.swift
+++ CaritasRayosX/RayosXCordinador/RayosXCordinador/CollapsibleView.swift
@@ -1,41 +1,47 @@
 //
 //  CollapsibleView.swift
-//  CaritasRayosX
+//  RayosXCordinador
 //
-//  Created by Alumno on 22/09/23.
+//  Created by Alumno on 26/09/23.
 //
 
 import SwiftUI
 
-struct Collapsible<Content: View>: View {
+struct CollapsibleView: View {
+    
     @State var label: () -> Text
     @State var content: () -> Content
-    
+        
     @State private var collapsed: Bool = true
     
+    
     var body: some View {
-        VStack {
-            Button(
-                action: { self.collapsed.toggle() },
-                label: {
-                    HStack {
-                        self.label()
-                        Spacer()
-                        Image(systemName: self.collapsed ? "chevron.down" : "chevron.up")
+        Button(
+                        action: { self.collapsed.toggle() },
+                        label: {
+                            HStack {
+                                self.label()
+                                Spacer()
+                                Image(systemName: self.collapsed ? "chevron.down" : "chevron.up")
+                            }
+                            .padding(.bottom, 1)
+                            .background(Color.white.opacity(0.01))
+                        }
+                    )
+                    .buttonStyle(PlainButtonStyle())
+                    
+                    VStack {
+                        self.content()
                     }
-                    .padding(.bottom, 1)
-                    .background(Color.white.opacity(0.01))
-                }
-            )
-            .buttonStyle(PlainButtonStyle())
-            
-            VStack {
-                self.content()
-            }
-            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: collapsed ? 0 : .none)
-            .clipped()
-            .animation(.easeOut)
-            .transition(.slide)
-        }
+                    .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: collapsed ? 0 : .none)
+                    .clipped()
+                    .animation(.easeOut)
+                    .transition(.slide)
+    }
+}
+
+struct CollapsibleView_Previews: PreviewProvider {
+    static var previews: some View {
+        CollapsibleView()
     }
 }
